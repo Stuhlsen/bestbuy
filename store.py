1c366d6384e807f5cc616e455c94baef992d926b
Stuhlsen <stuhlsen@proton.me>
2025-12-09 13:49:26 +0100

    Move application entry point to main.py

--- store.py
+++ store.py
@@ -62,20 +62,4 @@ class Store:
         return total_price
 
 
-def main():
-    product_list = [
-        products.Product("MacBook Air M2", price=1450, quantity=100),
-        products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
-        products.Product("Google Pixel 7", price=500, quantity=250),
-    ]
 
-    best_buy = Store(product_list)
-    available_products = best_buy.get_all_products()
-
-    print(best_buy.get_total_quantity())
-    print(best_buy.order([(available_products[0], 1),
-                          (available_products[1], 2)]))
-
-
-if __name__ == "__main__":
-    main()
